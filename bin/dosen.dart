9639109a0e4c9da8871db724c8b26b7995920a2a
Febri <febri.k150201@gmail.com>
2022-03-13 19:58:59 +0700

    Ver 1

--- bin/dosen.dart
+++
@@ -1,9 +0,0 @@
-import 'pegawai.dart';
-
-class dosen extends pegawai {
-  int bonusgaji = 0;
-  int jumlahsks = 0;
-
-  dosen(String? nama, int umur, int gaji, int totalgaji)
-      : super(nama, umur, gaji, totalgaji);
-}
--- bin/staff.dart
+++
@@ -1,7 +0,0 @@
-import 'pegawai.dart';
-
-class staff extends pegawai {
-  int jatahcuti = 12;
-
-  staff(String? nama, int umur) : super(nama, umur);
-}
